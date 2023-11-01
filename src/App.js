1ca4e9438faec20ed7fe13c88cea9f3e843b9fa6
AlinCodrut <alin.codrut.alin@gmail.com>
2023-11-01 10:28:34 +0100

    builder v1

--- src/App.js
+++ src/App.js
@@ -30,15 +30,24 @@ function App() {
       return // Stop further execution
     }
 
+    // Function to replace spaces with underscores
+    const replaceSpaces = input => input.replace(/ /g, "_")
+
+    // Replace spaces in input values
+    const updatedRefValue = replaceSpaces(refValue)
+    const updatedSource = replaceSpaces(source)
+    const updatedMedium = replaceSpaces(medium)
+    const updatedCampaign = replaceSpaces(campaign)
+
     // Ensuring base URL does not end with a slash or have query parameters
     let cleanUrl = url.replace(/\/?(\?.*)?$/, "")
 
     // Constructing final URL with proper encoding and syntax
     const queryParams = new URLSearchParams({
-      ref: refValue,
-      utm_source: source,
-      utm_medium: medium,
-      utm_campaign: campaign
+      ref: updatedRefValue,
+      utm_source: updatedSource,
+      utm_medium: updatedMedium,
+      utm_campaign: updatedCampaign
     }).toString()
 
     const final_url = `${cleanUrl}?${queryParams}`
@@ -49,9 +58,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>PlanB Campaign URL Builder</p>
+        <h1>PlanB Campaign URL Builder</h1>
       </header>
-      <form onSubmit={onSubmitUrl}>
+      {/* <form onSubmit={onSubmitUrl}>
         <label htmlFor="url">URL</label>
         <input type="text" id="url" value={url} onChange={e => setUrl(e.target.value)} required />
 
@@ -69,16 +78,99 @@ function App() {
         <button type="submit" class="btn btn-primary submit">
           Submit
         </button>
-      </form>
-      {finalUrl && (
-        <div className="final">
-          <h2 > <h2 style={{ color: 'red' }}>Url to share is: </h2>{finalUrl}</h2>
-          <button className="btn btn-secondary" onClick={copyToClipboard}>
-            Copy to Clipboard
-          </button>
-          {copySuccess && <div className="copy-message">{copySuccess}</div>}
+      </form> */}
+      <div className="columns">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-10 col-md-8 col-sm-12">
+              <form onSubmit={onSubmitUrl}>
+                <div className="mb-3">
+                  <label htmlFor="url" className="form-label">
+                    URL*
+                  </label>
+                  <input type="text" className="form-control" id="url" value={url} onChange={e => setUrl(e.target.value)} required />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="refValue" className="form-label">
+                    Ref Value (any custom name that describes the campaign)*
+                  </label>
+                  <input type="text" className="form-control" id="refValue" value={refValue} onChange={e => setRefValue(e.target.value)} required />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="source" className="form-label">
+                    Campaign Source*
+                  </label>
+                  <input type="text" className="form-control" id="source" value={source} onChange={e => setSource(e.target.value)} required />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="medium" className="form-label">
+                    Campaign Medium*
+                  </label>
+                  <input type="text" className="form-control" id="medium" value={medium} onChange={e => setMedium(e.target.value)} required />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="term" className="form-label">
+                    Campaign Name*
+                  </label>
+                  <input type="text" className="form-control" id="term" value={campaign} onChange={e => setCampaign(e.target.value)} required />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-      )}
+        <div className="container result">
+          <p>This tool allows you to easily add campaign parameters to URLs so you can measure Custom Campaigns in Matomo Analytics including the REF parameter. </p>
+          <p>Enter the website URL and campaign information Fill out all fields marked with an asterisk (*), and the campaign URL will be generated for you.</p>
+          <div className="utm-explanation">
+            <div >
+              <strong style={{ color: "white" }}>Campaign Source:</strong>
+              <p>
+                Identifies the source of your traffic, such as a search engine, newsletter, or other referral. <br />
+                <em>Example:</em> utm_source=newsletter
+              </p>
+            </div>
+
+            <div >
+              <strong style={{ color: "white" }}>Campaign Medium:</strong>
+              <p>
+                Describes the medium used to reach your site, such as email, CPC, social media, affiliate, or QR code. <br />
+                <em>Example:</em> utm_medium=email
+              </p>
+            </div>
+
+            <div >
+              <strong style={{ color: "white" }}>Campaign Name:</strong>
+              <p>
+                Specifies the individual campaign name, slogan, promo code, or specific promotion associated with your link. <br />
+                <em>Example:</em> utm_campaign=spring_sale
+              </p>
+            </div>
+
+          </div>
+          {finalUrl && (
+            <div className="final">
+              <div className="url-result">
+                <h2 style={{ color: "red" }}>Url to share is: </h2>
+                <a href={finalUrl} target="_blank" rel="noopener noreferrer">
+                  {finalUrl}
+                </a>
+              </div>
+              <button className="btn btn-secondary mt-3" onClick={copyToClipboard}>
+                Copy to Clipboard
+              </button>
+              {copySuccess && <div className="copy-message">{copySuccess}</div>}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
